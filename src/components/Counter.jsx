4060a54d82ef6c4f922a8a4b705731973e209620
Shubham Barore <shubham07barore@gmail.com>
2022-05-28 20:08:08 +0530

    added colors on even odd

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import style from "./Counter.module.css";
 
 const Counter = () => {
   const [count, setCount] = React.useState(0);
   return (
     <div>
-      counter : {count}
+      <h1 className={count % 2 ? style.red : style.green}>counter : {count}</h1>
       <button onClick={() => setCount(count + 1)}>up </button>
       <button onClick={() => setCount(count - 1)}>down</button>
       <button onClick={() => setCount(count * 2)}>double</button>
